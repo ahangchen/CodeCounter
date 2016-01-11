f996ed8f55d874f0d97d9a7168f4f59c11a37f16
ahangchen <1418659400@qq.com>
2016-01-11 21:18:15 +0800

    after move

--- utils/web/client.py
+++ utils/web/client.py
@@ -1,3 +1,24 @@
+import http.client
+
 __author__ = 'cwh'
 
 
+def get(url):
+    http_client = None
+    try:
+        http_client = http.client.HTTPConnection(url, 80, timeout=30)
+        http_client.request('GET', '/')
+
+        # response是HTTPResponse对象
+        response = http_client.getresponse()
+        print(response.status)
+        print(response.reason)
+        print(response.read())
+    except IOError:
+        print('error')
+    finally:
+        if http_client:
+            http_client.close()
+
+
+get('www.baidu.com')
