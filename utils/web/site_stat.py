dc088b355e9186f83b79742beba41f6e188dffc4
ahangchen <1418659400@qq.com>
2016-05-04 20:07:36 +0800

    all test

--- utils/web/site_stat.py
+++ utils/web/site_stat.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import random
 import time
+from urllib.error import HTTPError
 from urllib.request import urlopen
 
 from utils.concurent.thread_utils import run_in_background
@@ -20,10 +21,20 @@ random_size = 20
 
 def url_stat(url, test_type):
     start = time.time()  # 单位秒
-    resp = urlopen(url)
-    resp_msg = resp.read().decode()
-    resp_code = str(resp.code)
+    resp_code = 0
+    has_err = False
+    resp_msg = ""
+    try:
+        resp = urlopen(url)
+    except HTTPError as e:
+        has_err = True
+        resp_code = str(e.code)
+        resp_msg = e.msg
+
     end = time.time()
+    if not has_err:
+        resp_msg = resp.read().decode()
+        resp_code = str(resp.code)
     resp_time = str(end - start)
 
     sqlite.execute('url_stat.db', "insert into single (url, code, time, msg, type) values ('" +
@@ -100,7 +111,7 @@ def long_cache_stat():
 def random_play_back(starts, ends, sid):
     for i in range(random_size):
         # starts.append(datetime.datetime.now() - datetime.timedelta(seconds=random.randint(0, 345600)))
-        starts.append(datetime.datetime(2016, 4, 30) + datetime.timedelta(seconds=random.randint(0, 8640)))
+        starts.append(datetime.datetime(2016, 5, 1) + datetime.timedelta(seconds=random.randint(0, 86400)))
         ends.append(starts[i] + datetime.timedelta(seconds=15))
     for i in range(random_size):
         run_in_background(url_stat, urls[3] % (starts[i].year, starts[i].month, starts[i].day,
@@ -144,19 +155,36 @@ if __name__ == '__main__':
     # while i > 0:
     #     i -= 1
     #     session_stat()
+
     # 点播
     # play_back_stat()
+
     # 直播
     # real_play_stat()
+
     # 回播
     # long_play_stat()
+
     # 点播缓存测试
     # back_cache_stat()
+
     # 直播缓存测试
     # real_cache_stat()
+
     # 回播缓存测试
     # long_cache_stat()
+
     # 模拟正常流程测试
-    sample_stat()
-    input('请按回车退出')
-    print_data_set(query_stat_all())
+    # sample_stat()
+
+    # 模拟多客户端测试
+    # for i in range(1):
+    #     run_in_background(sample_stat)
+    # input('请按回车退出')
+    # print_data_set(query_stat_all())
+
+    # 模拟单客户端多次测试
+    for i in range(30):
+        sample_stat()
+        print('range : ' + str(i))
+        print_data_set(query_stat_all())
