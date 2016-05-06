c9d2ebaf2756460581edfde176f3aa8cd1768c8c
ahangchen <1418659400@qq.com>
2016-05-06 12:03:48 +0800

    full site stat

--- utils/web/site_stat.py
+++ utils/web/site_stat.py
@@ -74,8 +74,9 @@ def back_cache_stat():
     data = json.loads(url_stat(urls[0], "2"))
     sid = data['sid']
     url_stat(urls[3] % (2016, 5, 1, 12, 0, 0, 2016, 5, 1, 12, 0, 15, 3, 125, 216, 231, 164, 37777, sid), "1")
-    url_stat(urls[3] % (2016, 5, 1, 12, 0, 0, 2016, 5, 1, 12, 0, 15, 3, 125, 216, 231, 164, 37777, sid), "1")
-    url_stat(urls[2] % sid, "1")
+    url_stat(urls[1] % sid, "2")
+    url_stat(urls[3] % (2016, 5, 1, 12, 0, 0, 2016, 5, 1, 12, 0, 15, 3, 125, 216, 231, 164, 37777, sid), "2")
+    url_stat(urls[2] % sid, "2")
 
 
 def real_play_stat():
@@ -89,8 +90,9 @@ def real_cache_stat():
     data = json.loads(url_stat(urls[0], "2"))
     sid = data['sid']
     url_stat(urls[4] % (125, 216, 231, 164, 35556, 1, 125, 216, 231, 164, 37777, sid), "1")
-    url_stat(urls[4] % (125, 216, 231, 164, 35556, 1, 125, 216, 231, 164, 37777, sid), "1")
-    url_stat(urls[2] % sid, "1")
+    url_stat(urls[1] % sid, "2")
+    url_stat(urls[4] % (125, 216, 231, 164, 35556, 1, 125, 216, 231, 164, 37777, sid), "2")
+    url_stat(urls[2] % sid, "2")
 
 
 def long_play_stat():
@@ -104,87 +106,94 @@ def long_cache_stat():
     data = json.loads(url_stat(urls[0], "2"))
     sid = data['sid']
     url_stat(urls[5] % (2016, 5, 1, 12, 0, 0, 3, 125, 216, 231, 164, 37777, sid), "1")
-    url_stat(urls[5] % (2016, 5, 1, 12, 0, 0, 3, 125, 216, 231, 164, 37777, sid), "1")
-    url_stat(urls[2] % sid, "1")
+    url_stat(urls[1] % sid, "2")
+    url_stat(urls[5] % (2016, 5, 1, 12, 0, 0, 3, 125, 216, 231, 164, 37777, sid), "2")
+    url_stat(urls[2] % sid, "2")
 
 
-def random_play_back(starts, ends, sid):
-    for i in range(random_size):
+def random_play_back(starts, ends, sid, test_type):
+    for k in range(random_size):
         # starts.append(datetime.datetime.now() - datetime.timedelta(seconds=random.randint(0, 345600)))
-        starts.append(datetime.datetime(2016, 5, 1) + datetime.timedelta(seconds=random.randint(0, 86400)))
-        ends.append(starts[i] + datetime.timedelta(seconds=15))
-    for i in range(random_size):
-        run_in_background(url_stat, urls[3] % (starts[i].year, starts[i].month, starts[i].day,
-                                               starts[i].hour, starts[i].minute, starts[i].second,
-                                               ends[i].year, ends[i].month, ends[i].day,
-                                               ends[i].hour, ends[i].minute, ends[i].second, random.randint(2, 4),
-                                               125, 216, 231, 164, 37777, sid), "3")
+        starts.append(datetime.datetime(2016, 5, 5) + datetime.timedelta(seconds=random.randint(0, 43200)))
+        ends.append(starts[k] + datetime.timedelta(seconds=15))
+    for k in range(random_size):
+        run_in_background(url_stat, urls[3] % (starts[k].year, starts[k].month, starts[k].day,
+                                               starts[k].hour, starts[k].minute, starts[k].second,
+                                               ends[k].year, ends[k].month, ends[k].day,
+                                               ends[k].hour, ends[k].minute, ends[k].second, random.randint(2, 4),
+                                               125, 216, 231, 164, 37777, sid), test_type)
         time.sleep(0.05)
 
 
-def sample_stat():
+def sample_stat(test_type):
     # 登录
-    data = json.loads(url_stat(urls[0], "3"))
+    data = json.loads(url_stat(urls[0], test_type))
     sid = data['sid']
 
     # 直播
-    url_stat(urls[4] % (125, 216, 231, 164, 35556, 1, 125, 216, 231, 164, 37777, sid), "3")
+    url_stat(urls[4] % (125, 216, 231, 164, 35556, 1, 125, 216, 231, 164, 37777, sid), test_type)
     # 点播抓取多个随机视频
     starts = []
     ends = []
-    random_play_back(starts, ends, sid)
+    random_play_back(starts, ends, sid, test_type)
     time.sleep(15)
-
+    url_stat(urls[1] % sid, test_type)
     # 随机挑一个来回播
     url_stat(urls[5] % (
         starts[random.randint(0, random_size - 1)].year, starts[random.randint(0, random_size - 1)].month,
         starts[random.randint(0, random_size - 1)].day,
         starts[random.randint(0, random_size - 1)].hour, starts[random.randint(0, random_size - 1)].minute,
         starts[random.randint(0, random_size - 1)].second,
-        random.randint(2, 4), 125, 216, 231, 164, 37777, sid), "3")
+        random.randint(2, 4), 125, 216, 231, 164, 37777, sid), test_type)
     # 点播抓取多个随机视频
-    random_play_back(starts, ends, sid)
+    random_play_back(starts, ends, sid, test_type)
     time.sleep(15)
-    url_stat(urls[2] % sid, "1")
+    url_stat(urls[2] % sid, test_type)
 
 
 if __name__ == '__main__':
     prepare()
-    # 顺序请求测试平均时间
-    # i = 10
-    # while i > 0:
-    #     i -= 1
-    #     session_stat()
 
-    # 点播
-    # play_back_stat()
+    for i in range(30):
+        # 会话相关测试
+        session_stat()
+        # 点播
+        play_back_stat()
 
-    # 直播
-    # real_play_stat()
+        # 直播
+        real_play_stat()
 
-    # 回播
-    # long_play_stat()
+        # 回播
+        long_play_stat()
 
-    # 点播缓存测试
-    # back_cache_stat()
+        # 点播缓存测试
+        back_cache_stat()
 
-    # 直播缓存测试
-    # real_cache_stat()
+        # 直播缓存测试
+        real_cache_stat()
 
-    # 回播缓存测试
-    # long_cache_stat()
+        # 回播缓存测试
+        long_cache_stat()
+
+        print_data_set(query_stat_all())
 
     # 模拟正常流程测试
-    # sample_stat()
-
-    # 模拟多客户端测试
-    # for i in range(1):
-    #     run_in_background(sample_stat)
-    # input('请按回车退出')
-    # print_data_set(query_stat_all())
+    sample_stat("3")
 
     # 模拟单客户端多次测试
     for i in range(30):
-        sample_stat()
-        print('range : ' + str(i))
+        sample_stat("3")
         print_data_set(query_stat_all())
+        print('range : ' + str(i))
+
+    # 模拟多客户端测试
+    for i in range(4):
+        run_in_background(sample_stat, "4")
+    sample_stat("4")
+    input('请按回车退出')
+    print_data_set(query_stat_all())
+
+    # 测试删
+    # sqlite.execute('url_stat.db', 'delete from single WHERE id > 2099')
+    # 测试改
+    # sqlite.execute('url_stat.db', 'update single set type = 3 WHERE type = 1')
