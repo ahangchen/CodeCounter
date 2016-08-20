c328306cd2713b7041b3f98b057df52e7e9b70ad
ahangchen <1418659400@qq.com>
2016-08-20 14:00:58 +0800

    bugfix

--- utils/web/client.py
+++ utils/web/client.py
@@ -1,10 +1,37 @@
+# coding=utf-8
 import http.client
 import logging
+import urllib3
 
 __author__ = 'cwh'
 
 
 def get(url):
+    """"
+    :param url 比如，110.65.10.209/uploadfile，要有host和后半部分
+    如果请求的确实只有host，就传host就好 110.65.10.209
+    :return 返回response对象
+    """
+    strings = url.split('/')
+    host = strings[0].split(':')[0]
+    port = strings[0].split(':')[1]
+    child = url.replace(strings[0] + '/', '')
+    http_client = None
+    try:
+        http_client = http.client.HTTPConnection(host, port, timeout=30)
+        http_client.request('GET', child)
+
+        ret = http_client.getresponse()
+        return ret
+    except IOError as e:
+        print(url)
+        logging.exception(e)
+    finally:
+        if http_client:
+            http_client.close()
+
+
+def post(url):
     """"
     :param url 比如，110.65.10.209/uploadfile，要有host和后半部分
     如果请求的确实只有host，就传host就好 110.65.10.209
